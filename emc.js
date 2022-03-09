7c6782e8247f4f660e9b239c5d9b4c341e3ee40c
mk-pmb <mk-pmb@users.noreply.github.com>
2022-03-09 18:36:44 +0100

    v0.1.2: Fix config accumulator

--- emc.js
+++ emc.js
@@ -5,10 +5,10 @@
 var EX, mo = require('merge-options'), eid = require('envcfg-import-deep-pmb');
 
 EX = function envcfgMergeConfigs(opt, configs) {
-  var dest = (this || {});
   configs = Array.prototype.slice.call(arguments, 1);
   configs = Array.prototype.concat.apply([], configs);
-  mo.apply(dest, configs);
+  var dest = mo.apply(null, configs);
+  console.debug(dest);
   opt = Object.assign({}, opt);
   opt.inplace = true;
   eid(dest, opt);
